7a11ccc7510d5ad305c57e4c5035f1ac894055dc
Jeremy Ellis <keyfreemusic@gmail.com>
2022-02-03 05:55:13 -0800

    Add files via upload

---
+++ temp-build-a-files/firmware-arduino-portenta-h7.ino.cpp
@@ -0,0 +1,53 @@
+#include <Arduino.h>
+#line 1 "/root/Arduino/a-portenta-boot/firmware-arduino-portenta-h7/firmware-arduino-portenta-h7.ino"
+/* Edge Impulse ingestion SDK
+ * Copyright (c) 2020 EdgeImpulse Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/* Include ----------------------------------------------------------------- */
+#include "mbed.h"
+#include "src/ei_main.h"
+
+
+/* Private variables ------------------------------------------------------- */
+static rtos::Thread mbed_thread(osPriorityAboveNormal, 8 * 1024);
+
+/* Public functions -------------------------------------------------------- */
+
+#line 33 "/root/Arduino/a-portenta-boot/firmware-arduino-portenta-h7/firmware-arduino-portenta-h7.ino"
+void setup();
+#line 42 "/root/Arduino/a-portenta-boot/firmware-arduino-portenta-h7/firmware-arduino-portenta-h7.ino"
+void loop();
+#line 33 "/root/Arduino/a-portenta-boot/firmware-arduino-portenta-h7/firmware-arduino-portenta-h7.ino"
+void setup() {
+
+    Serial.begin(115200);
+
+    ei_main();
+
+    mbed_thread.start(&ei_main);
+}
+
+void loop() {
+    // delay will pause the thread, so the loop won't take any cycles away from us here
+    delay(10000);
+}
+
